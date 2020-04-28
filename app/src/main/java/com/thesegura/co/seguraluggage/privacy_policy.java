69a07541009688dfde458aade240fdbc3c587493
Abhishek-kumar09 <abhimait1909@gmail.com>
2020-04-29 02:56:58 +0530

    Added privacy policies

---
+++ app/src/main/java/com/thesegura/co/seguraluggage/privacy_policy.java
@@ -0,0 +1,25 @@
+package com.thesegura.co.seguraluggage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
+
+public class privacy_policy extends AppCompatActivity {
+    Toolbar toolbar;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy_policy);
+
+        textView = findViewById(R.id.privacyText);
+        textView.setMovementMethod(new ScrollingMovementMethod());
+
+        toolbar = findViewById(R.id.toolBarOthers);
+        toolbar.setTitle("Segura Privacy Policy");
+    }
+}
